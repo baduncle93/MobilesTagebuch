8d7b52569028abbd12be2b62bf68e01c418120de
baduncle93 <38860973+baduncle93@users.noreply.github.com>
2018-05-03 12:36:35 +0200

    first commit
    first commit

---
+++ app/src/main/java/com/example/baduncle/thediary/Kalenderansicht.java
@@ -0,0 +1,49 @@
+package com.example.baduncle.thediary;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Kalenderansicht extends AppCompatActivity {
+    final Context context=this;
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_liste:
+                    mTextMessage.setText("Liste");
+                    Intent intent1 = new Intent(context,Listenansicht.class);
+                    startActivity(intent1);
+                    return true;
+                case R.id.navigation_kalender:
+                    mTextMessage.setText("Kalender");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kalenderansicht);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Menu menu = navigation.getMenu();
+        MenuItem menuitem= menu.getItem(1);
+        menuitem.setChecked(true);
+    }
+
+}
---
+++ app/src/main/java/com/example/baduncle/thediary/Listenansicht.java
@@ -0,0 +1,49 @@
+package com.example.baduncle.thediary;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Listenansicht extends AppCompatActivity {
+    final Context context=this;
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_liste:
+                    mTextMessage.setText("Liste");
+                    return true;
+                case R.id.navigation_kalender:
+                    mTextMessage.setText("Kalender");
+                    Intent intent2 = new Intent(context,Kalenderansicht.class);
+                    startActivity(intent2);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listenansicht);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Menu menu = navigation.getMenu();
+        MenuItem menuitem= menu.getItem(0);
+        menuitem.setChecked(true);
+    }
+
+}
