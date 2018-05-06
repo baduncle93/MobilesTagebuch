9728b1744cef45547811ff27b4531c176bcae01c
Thahn-y <a01636183@unet.univie.ac.at>
2018-05-06 16:07:24 +0200

    rating bar and search

--- app/src/main/java/com/example/baduncle/thediary/Listenansicht.java
+++ app/src/main/java/com/example/baduncle/thediary/Listenansicht.java
@@ -1,10 +1,8 @@
 package com.example.baduncle.thediary;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
@@ -12,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class Listenansicht extends AppCompatActivity {
@@ -33,6 +30,13 @@ public class Listenansicht extends AppCompatActivity {
                     mTextMessage.setText("");
                     Intent intent2 = new Intent(context,Kalenderansicht.class);
                     startActivity(intent2);
+                    overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                    return true;
+
+                case R.id.navigation_search:
+                    mTextMessage.setText("");
+                    Intent intent3 = new Intent(context,SearchActivity.class);
+                    startActivity(intent3);
                     overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
                     return true;
             }
--- app/src/main/java/com/example/baduncle/thediary/SearchActivity.java
+++ app/src/main/java/com/example/baduncle/thediary/SearchActivity.java
@@ -5,16 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
-public class Kalenderansicht extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity {
     final Context context = this;
-    private TextView mTextMessage;
+  //  private TextView mTextMessage;
 
     //Navigation mittels Navigationsleiste unten
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -24,20 +21,17 @@ public class Kalenderansicht extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_liste:
-                    mTextMessage.setText("");
                     Intent intent1 = new Intent(context,Listenansicht.class);
                     startActivity(intent1);
                     overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
                     return true;
                 case R.id.navigation_kalender:
-                    mTextMessage.setText("");
+                    Intent intent3 = new Intent(context,Kalenderansicht.class);
+                    startActivity(intent3);
+                    overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
                     return true;
 
                 case R.id.navigation_search:
-                    mTextMessage.setText("");
-                    Intent intent3 = new Intent(context,SearchActivity.class);
-                    startActivity(intent3);
-                    overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
                     return true;
             }
             return false;
@@ -47,26 +41,15 @@ public class Kalenderansicht extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kalenderansicht);
-        FloatingActionButton button=  findViewById(R.id.addbutton_kal);
-        mTextMessage = (TextView) findViewById(R.id.message);
+        setContentView(R.layout.activity_search);
 
         //Navigationsleiste initialisieren
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         Menu menu = navigation.getMenu();
-        MenuItem menuitem= menu.getItem(1);
+        MenuItem menuitem= menu.getItem(2);
         menuitem.setChecked(true);
 
-        //Neuer Eintrag Activity mit Add-Button öffnen
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent= new Intent(context,NeuerEintrag.class);
-                startActivity(intent);
-                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
-            }
-        }) ;
     }
 
 }
