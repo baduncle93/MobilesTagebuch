df94eed491464f75df1fbb03cec930a44d5ac6c7
baduncle93 <38860973+baduncle93@users.noreply.github.com>
2018-05-06 16:33:20 +0200

    Revert "added edit activity / but doesnt work yet"
    This reverts commit be8cc9772a40570d0fb91f46159cdeb25905fcf3.

--- app/src/main/java/com/example/baduncle/thediary/Detailansicht.java
+++ app/src/main/java/com/example/baduncle/thediary/Detailansicht.java
@@ -51,9 +51,9 @@ public class Detailansicht extends AppCompatActivity {
         detailbearb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(context,EintragEdit.class);
-                intent.putExtra("editeintrag",alledaten.get(index).toString());
-                intent.putExtra("index",index);
+                Intent intent= new Intent(context,Listenansicht.class);
+
+
                 startActivity(intent);
                 overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
             }
