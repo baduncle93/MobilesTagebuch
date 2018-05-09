abbc598960dce0176bdacf386b89c22c36e5cf86
Michael Hellstern <a01637853@unet.univie.ac.at>
2018-05-09 18:46:53 +0200

    bad window token

--- app/src/main/java/com/example/baduncle/thediary/SearchResult.java
+++ app/src/main/java/com/example/baduncle/thediary/SearchResult.java
@@ -1,5 +1,6 @@
 package com.example.baduncle.thediary;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -29,50 +30,55 @@ public class SearchResult extends AppCompatActivity {
 
     int sterne;
     int preis;
+    String titel;
 
     private List<Datensammler> alledaten;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_result);
-        suchListe = findViewById(R.id.suchListe);
-        neu =getIntent();
-        extras=neu.getExtras();
-        adapter = new CustomAdapter(context,R.layout.custom_row);
-        suchListe.setAdapter(adapter);
-        eintragsspeicher = getSharedPreferences("Eintragsspeicher",MODE_PRIVATE);
-        eintragseditor = eintragsspeicher.edit();
 
-        //Shows Logo
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setLogo(R.mipmap.ic_launcher);
-        actionBar.setDisplayUseLogoEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(true);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_search_result);
+            suchListe = findViewById(R.id.suchListe);
+            neu = getIntent();
+            extras = neu.getExtras();
+            adapter = new CustomAdapter(context, R.layout.custom_row);
 
-        Intent intent = getIntent();
-        preis = intent.getIntExtra("preis", 0);
-        sterne = intent.getIntExtra("sterne", 0);
+            suchListe.setAdapter(adapter);
+            eintragsspeicher = getSharedPreferences("Eintragsspeicher", MODE_PRIVATE);
+            eintragseditor = eintragsspeicher.edit();
 
-        Toast.makeText(context,"Preis: " + preis + ", Sterne: " + sterne ,Toast.LENGTH_SHORT).show();
+            //Shows Logo
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setLogo(R.mipmap.ic_launcher);
+            actionBar.setDisplayUseLogoEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
 
-        suchListe.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            Intent intent = getIntent();
+            preis = intent.getIntExtra("preis", 0);
+            sterne = intent.getIntExtra("sterne", 0);
+            titel = intent.getStringExtra("titel");
+
+            Toast.makeText(context, "Preis: " + preis + ", Sterne: " + sterne, Toast.LENGTH_SHORT).show();
+
+            suchListe.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                     Intent intent = new Intent(context, Detailansicht.class);
                     intent.putExtra("index", i);
                     intent.putExtra("eintragsid", suchListe.getItemAtPosition(i).toString());
                     startActivity(intent);
+                }
+            });
+
+            alledaten = new ArrayList<Datensammler>();
+            alledaten = Datensammler.parseEntries(eintragsspeicher.getString("Einträge", "0§Beispieltitel§Beispieltext§" + Uri.parse("android.resource://com.example.baduncle.thediary/drawable/defaultpicture") + "§01.01.2000§0§0%"));
+            for (int i = 0; alledaten.size() > i; i++) {
+                if (alledaten.get(i).getPreis() >= preis && alledaten.get(i).getSterne() >= sterne && alledaten.get(i).getTitel().contains(titel))
+                    adapter.add(alledaten.get(i));
             }
-        });
 
-        alledaten = new ArrayList<Datensammler>();
-        alledaten = Datensammler.parseEntries(eintragsspeicher.getString("Einträge","0§Beispieltitel§Beispieltext§"+ Uri.parse("android.resource://com.example.baduncle.thediary/drawable/defaultpicture")+"§01.01.2000§0§0%"));
-        for(int i=0;alledaten.size()>i;i++){
-            if(alledaten.get(i).getPreis()>=preis && alledaten.get(i).getSterne()>=sterne)
-                adapter.add(alledaten.get(i));
+
         }
-
-    }
 
 
 }
